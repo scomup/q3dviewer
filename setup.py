5688373d626c77322206c7ece74e503403c54bb0
liu.yang <scomup@gmail.com>
2025-01-22 11:45:27 +0900

    remove vscode

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='q3dviewer',
-    version='1.0.2',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=[
         'numpy',
