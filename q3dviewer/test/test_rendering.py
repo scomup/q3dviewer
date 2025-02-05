2bd47cef6e4397d27b6949b870a859bef3256d35
liu.yang <liu.yang@jp.panasonic.com>
2025-02-05 09:33:59 +0900

    test workflow

--- q3dviewer/test/test_rendering.py
+++ q3dviewer/test/test_rendering.py
@@ -14,28 +14,41 @@ import numpy as np
 import q3dviewer as q3d
 import cv2
 
-cloud, _ = q3d.load_pcd('/home/liu/lab.pcd')
+cloud, _ = q3d.load_pcd('//home/liu/tmp/gs_slam_dataset/data.pcd')
 
+pose = np.array([0.00705215, 0.00596959, 0.00128543, 0.00296279, 0.0518244, -0.0033289, 0.998646])
 
-Tcw = np.array([[7.07106781e-01,  7.07106781e-01,  0.00000000e+00,
-                 0.00000000e+00],
-                [-3.53553391e-01,  3.53553391e-01,  8.66025404e-01,
-                 3.55271368e-15],
-                [6.12372436e-01, -6.12372436e-01,  5.00000000e-01,
-                 -4.00000000e+01],
-                [0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                 1.00000000e+00]])
-# convert the opengl camera coordinate to the opencv camera coordinate
-Tconv = np.array([[1, 0, 0, 0],
-                  [0, -1, 0, 0],
-                  [0, 0, -1, 0],
-                  [0, 0, 0, 1]])
+def get_T(pose):
+    t = pose[:3]
+    quat = pose[3:]
+    R = q3d.quaternion_to_matrix(quat)
+    T = q3d.makeT(R, t)
+    return T
 
-Tcw = Tconv @ Tcw
+Twl = get_T(pose)
 
-K = np.array([[1.64718029e+03, 0.00000000e+00, 9.51000000e+02],
-              [0.00000000e+00, 1.64718036e+03, 5.31000000e+02],
-              [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+pose_cl = np.array([-0.07903228, -0.00545123, -0.04363822, 0.48705093, -0.46590715,  0.51408928,  0.53049424])
+
+Tcl = get_T(pose_cl)
+
+Tcw = Tcl @ np.linalg.inv(Twl)
+
+# # convert the opengl camera coordinate to the opencv camera coordinate
+# Tconv = np.array([[1, 0, 0, 0],
+#                   [0, -1, 0, 0],
+#                   [0, 0, -1, 0],
+#                   [0, 0, 0, 1]])
+# 
+# Tcw = Tconv @ Tcw
+
+cam_params = np.array([409.66105605989907, 409.32179199376003, 436.5, 193.0])
+
+width=873
+height=386
+
+K = np.array([[cam_params[0], 0, cam_params[2]],
+                [0, cam_params[1], cam_params[3]],
+                [0, 0, 1]])
 
 
 def render_frame(cloud, Tcw, K, width, height):
@@ -44,8 +57,9 @@ def render_frame(cloud, Tcw, K, width, height):
     pc = (Rcw @ cloud['xyz'].T).T + tcw
     uv = (K @ pc.T).T
     uv = uv[:, :2] / uv[:, 2][:, np.newaxis]
-    mask = (pc[:, 2] > 0) & (uv[:, 0] > 0) & (
-        uv[:, 0] < width) & (uv[:, 1] > 0) & (uv[:, 1] < height)
+    mask = (pc[:, 2] > 0) & (pc[:, 2] < 60) & \
+           (uv[:, 0] > 0) & (uv[:, 0] < width) & \
+           (uv[:, 1] > 0) & (uv[:, 1] < height)
     uv = uv[mask]
     u = uv[:, 0].astype(int)
     v = uv[:, 1].astype(int)
@@ -53,20 +67,24 @@ def render_frame(cloud, Tcw, K, width, height):
     r = rgb >> 16 & 0xff
     g = rgb >> 8 & 0xff
     b = rgb & 0xff
+    i = rgb >> 24 & 0xff
+
+    rgb = q3d.rainbow(i, scalar_min=0, scalar_max=50)[:, [2, 1, 0]]
 
     # Sort by depth to ensure front points are drawn first
     depth = pc[mask, 2]
     sorted_indices = np.argsort(depth)
     u = u[sorted_indices]
     v = v[sorted_indices]
-    r = r[sorted_indices]
-    g = g[sorted_indices]
-    b = b[sorted_indices]
+    image[v, u] = rgb[sorted_indices]
+    # r = r[sorted_indices]
+    # g = g[sorted_indices]
+    # b = b[sorted_indices]
+    # image[v, u] = np.stack([b, g, r], axis=1)
 
-    image[v, u] = np.stack([b, g, r], axis=1)
     return image
 
 
-image = render_frame(cloud, Tcw, K, 1902, 1062)
+image = render_frame(cloud, Tcw, K, width, height)
 cv2.imshow('image', image)
 cv2.waitKey(0)
